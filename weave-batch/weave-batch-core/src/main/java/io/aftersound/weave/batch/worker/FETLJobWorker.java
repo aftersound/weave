8ab9c106cdbbc9f42bf6137971b6195c6d3d3376
aftersound <aftersound@gmail.com>
2019-10-27 07:19:13 -0700

    weave-batch-core FETLJobWorker half-baked

--- weave-batch/weave-batch-core/src/main/java/io/aftersound/weave/batch/worker/FETLJobWorker.java
+++ weave-batch/weave-batch-core/src/main/java/io/aftersound/weave/batch/worker/FETLJobWorker.java
@@ -1,9 +1,53 @@
 package io.aftersound.weave.batch.worker;
 
+import io.aftersound.weave.batch.ResourceTypes;
 import io.aftersound.weave.batch.jobspec.fetl.FETLJobSpec;
+import io.aftersound.weave.common.Result;
+import io.aftersound.weave.config.Config;
+import io.aftersound.weave.filehandler.FileHandler;
+import io.aftersound.weave.filehandler.FileHandlerFactory;
+import io.aftersound.weave.filehandler.FileHandlingControl;
 import io.aftersound.weave.resources.ManagedResources;
+import io.aftersound.weave.resources.ResourceInitializer;
+import io.aftersound.weave.resources.ResourceType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import static io.aftersound.weave.common.Keys.RETURN_INFO;
+import static io.aftersound.weave.common.Keys.RETURN_INFOS;
+import static io.aftersound.weave.filehandler.FileReturnInfoKeys.FILE_LIST;
+import static io.aftersound.weave.filehandler.FileReturnInfoKeys.TARGET_FILE_PATH;
 
 public class FETLJobWorker extends JobWorker<FETLJobSpec> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FETLJobWorker.class);
+
+    public static final ResourceInitializer RESOURCE_INITIALIZER = new ResourceInitializer() {
+
+        @Override
+        public ResourceType<?>[] getDependingResourceTypes() {
+            return new ResourceType[] {
+                    ResourceTypes.FILE_HANDLER_FACTORY,
+                    ResourceTypes.JOB_DATA_DIR
+            };
+        }
+
+        @Override
+        public ResourceType<?>[] getShareableResourceTypes() {
+            return new ResourceType[0];
+        }
+
+        @Override
+        public ResourceType<?>[] getResourceTypes() {
+            return new ResourceType[0];
+        }
+
+        @Override
+        public void initializeResources(ManagedResources managedResources, Config resourceConfig) throws Exception {
+        }
+    };
 
     public FETLJobWorker(ManagedResources managedResources, FETLJobSpec jobSpec) {
         super(managedResources, jobSpec);
@@ -11,6 +55,33 @@ public class FETLJobWorker extends JobWorker<FETLJobSpec> {
 
     @Override
     public void execute() throws Exception {
+        FileHandlerFactory fileHandlerFactory = managedResources.getResource(ResourceTypes.FILE_HANDLER_FACTORY);
+        //   Set up local directory that holds data files to be copied from data source
+        String localDataDir = managedResources.getResource(ResourceTypes.JOB_DATA_DIR);
 
+        FileHandlingControl sourceFileHandlingControl = jobSpec.getSourceFileHandlingControl();
+        FileHandler sourceFileHandler = fileHandlerFactory.getFileHandler(sourceFileHandlingControl);
+        LOGGER.info("Using fileHandler: " + sourceFileHandler);
+
+        Result result = sourceFileHandler.listFiles();
+        if (result.isFailure()) {
+            throw new Exception(result.getFailureReason());
+        }
+
+        List<String> filePaths = (List<String>)(result.get(RETURN_INFO).get(FILE_LIST));
+
+        Result filesCopyResult = sourceFileHandler.copyFilesFrom(filePaths, localDataDir);
+        if (filesCopyResult.isFailure()) {
+            throw new Exception(result.getFailureReason());
+        }
+
+        if (filesCopyResult.isPartialFailure()) {
+            throw new Exception(result.getMessage());
+        }
+
+        List<String> localFilePaths = filesCopyResult.get(RETURN_INFOS).extractValues(TARGET_FILE_PATH);
+
+        // ETL with data file available at local
+        // TODO
     }
 }
--- weave-batch/weave-batch-spring/src/main/java/io/aftersound/weave/batch/Constants.java
+++ weave-batch/weave-batch-spring/src/main/java/io/aftersound/weave/batch/Constants.java
@@ -2,17 +2,9 @@ package io.aftersound.weave.batch;
 
 import io.aftersound.weave.batch.jobspec.JobSpec;
 import io.aftersound.weave.batch.worker.JobWorker;
-import io.aftersound.weave.dataclient.DataClientRegistry;
 import io.aftersound.weave.resources.ResourceType;
 
 class Constants {
-
-    // Namespaces
-    static final String NS_JOB_SPEC = "job-spec";
-
-    // Keys of Job Parameters
-    static final String JP_JOB_SPEC = "--jobSpec";
-
     // Keys of Managed Resources
     static final ResourceType<String> JOB_NAME = new ResourceType<>("JOB_NAME", String.class);
     static final ResourceType<JobSpec> JOB_SPEC = new ResourceType<>("JOB_SPEC", JobSpec.class);
@@ -20,15 +12,4 @@ class Constants {
     static final ResourceType<String> JOB_DATA_DIR = new ResourceType<>("JOB_DATA_DIR", String.class);
     static final ResourceType<String> JOB_LOG_DIR = new ResourceType<>("JOB_LOG_DIR", String.class);
     static final ResourceType<JobWorker> JOB_WORKER = new ResourceType<>("JOB_WORKER", JobWorker.class);
-    static final ResourceType<JobSpec> WORKER_JOB_SPEC = new ResourceType<>("WORKER_JOB_SPEC", JobSpec.class);
-
-
-    static final String MR_JOB_SPEC = "JOB_SPEC";
-    static final String MR_WORKER_JOB_SPEC = "WORKER_JOB_SPEC";
-
-    // Keys of System Properties
-    static final String SP_JOB_NAME = "jobName";
-
-    // Default values
-    static final String DEFAULT_WORK_DIR = "${java.io.tmpdir}/weave";
 }
