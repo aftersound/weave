aa05ab0012372ea23cbfbe55e1b766fddfc8d070
aftersound <aftersound@gmail.com>
2019-11-05 22:08:43 -0800

    weave-service-spring WeaveServiceSecurityConfiguration experimental code

--- weave-service-spring/src/main/java/io/aftersound/weave/service/WeaveServiceSecurityConfiguration.java
+++ weave-service-spring/src/main/java/io/aftersound/weave/service/WeaveServiceSecurityConfiguration.java
@@ -3,11 +3,17 @@ package io.aftersound.weave.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
 
 // https://spring.io/guides/topicals/spring-security-architecture/
 @Configuration
+@EnableWebSecurity
 public class WeaveServiceSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -16,14 +22,31 @@ public class WeaveServiceSecurityConfiguration extends WebSecurityConfigurerAdap
     @Autowired
     WeavePrivilegeEvaluator weavePrivilegeEvaluator;
 
-    @Override
-    public void configure(AuthenticationManagerBuilder builder) throws Exception {
-        builder.authenticationProvider(weaveAuthenticationProvider);
-    }
+//    @Override
+//    public void configure(AuthenticationManagerBuilder builder) throws Exception {
+//         builder.authenticationProvider(weaveAuthenticationProvider);
+//    }
+
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//         web.privilegeEvaluator(weavePrivilegeEvaluator);
+//    }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.privilegeEvaluator(weavePrivilegeEvaluator);
+    public void configure(HttpSecurity http) throws Exception {
+        /* below snippet has conditional behavior
+        http.requestMatcher(new RequestMatcher() {
+            @Override
+            public boolean matches(HttpServletRequest request) {
+                String requestURI = request.getRequestURI();
+                return "/admin/service/extension/list".equals(requestURI);
+            }
+        }).authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        */
+//        httpSecurity.authenticationProvider(weaveAuthenticationProvider);
+//        http.authorizeRequests().expressionHandler().
+//        http.authorizeRequests().anyRequest().authenticated()
+//                .and().httpBasic();
     }
 
 }
