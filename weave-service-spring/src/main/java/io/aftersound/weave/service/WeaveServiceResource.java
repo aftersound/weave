10841ae82d36bdda189ff4933935607b0fe0bd17
aftersound <aftersound@gmail.com>
2019-11-16 20:56:10 -0800

    weave-service-spring introduce HttpServletRequestWrapper
    to avoid
    java.lang.IllegalStateException: getInputStream() has already been called for this request

--- weave-service-spring/src/main/java/io/aftersound/weave/service/WeaveServiceResource.java
+++ weave-service-spring/src/main/java/io/aftersound/weave/service/WeaveServiceResource.java
@@ -4,20 +4,13 @@ import io.aftersound.weave.actor.ActorRegistry;
 import io.aftersound.weave.cache.CacheRegistry;
 import io.aftersound.weave.cache.KeyGenerator;
 import io.aftersound.weave.service.request.Deriver;
+import io.aftersound.weave.service.request.HttpServletRequestWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.HEAD;
-import javax.ws.rs.OPTIONS;
-import javax.ws.rs.PATCH;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -57,65 +50,72 @@ public class WeaveServiceResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response get(@Context HttpServletRequest request) {
-        return serve(request);
+        return serve(request, null);
     }
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response post(@Context HttpServletRequest request) {
-        return serve(request);
+    public Response post(@Context HttpServletRequest request, String entity) {
+        return serve(request, entity);
     }
 
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
     public Response delete(@Context HttpServletRequest request) {
-        return serve(request);
+        return serve(request, null);
     }
 
     @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response put(@Context HttpServletRequest request) {
-        return serve(request);
+    public Response put(@Context HttpServletRequest request, String entity) {
+        return serve(request, entity);
     }
 
     @PATCH
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response patch(@Context HttpServletRequest request) {
-        return serve(request);
+    public Response patch(@Context HttpServletRequest request, String entity) {
+        return serve(request, entity);
     }
 
     @HEAD
     @Produces(MediaType.APPLICATION_JSON)
     public Response head(@Context HttpServletRequest request) {
-        return serve(request);
+        return serve(request, null);
     }
 
     @OPTIONS
     @Produces(MediaType.APPLICATION_JSON)
     public Response options(@Context HttpServletRequest request) {
-        return serve(request);
+        return serve(request, null);
     }
 
-    private Response serve(HttpServletRequest request) {
+    private Response serve(HttpServletRequest request, String entity) {
         String requestURI = request.getRequestURI();
         boolean isAdminServiceRequest = (requestURI.startsWith("/admin/") || requestURI.startsWith("/openapi"));
+
+        ServiceDelegate serviceDelegate;
         if (isAdminServiceRequest) {
-            return new ServiceDelegate(
+            serviceDelegate = new ServiceDelegate(
                     adminServiceMetadataManager,
                     adminServiceExecutorFactory,
                     paramDeriverRegistry,
                     cacheRegistry,
                     cacheKeyGeneratorRegistry
-            ).serve(request);
+            );
         } else {
-            return new ServiceDelegate(
+            serviceDelegate = new ServiceDelegate(
                     serviceMetadataManager,
                     serviceExecutorFactory,
                     paramDeriverRegistry,
                     cacheRegistry,
                     cacheKeyGeneratorRegistry
-            ).serve(request);
+            );
         }
+
+        return serviceDelegate.serve(new HttpServletRequestWrapper(request, entity));
     }
 
 }
