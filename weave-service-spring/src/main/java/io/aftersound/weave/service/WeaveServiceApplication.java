715199eb8d452262fe51e27007f260d50cfee698
aftersound <aftersound@gmail.com>
2019-12-02 00:15:22 -0800

    weave-service-spring prevent CassandraDataAutoConfiguration

--- weave-service-spring/src/main/java/io/aftersound/weave/service/WeaveServiceApplication.java
+++ weave-service-spring/src/main/java/io/aftersound/weave/service/WeaveServiceApplication.java
@@ -2,8 +2,13 @@ package io.aftersound.weave.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude={
+			CassandraDataAutoConfiguration.class
+		}
+)
 public class WeaveServiceApplication {
 
 	public static void main(String[] args) {
