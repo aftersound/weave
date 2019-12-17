7f8c6c675e0fccc4e98db75557e860fc71d61499
aftersound <aftersound@gmail.com>
2019-12-17 09:41:53 -0800

    weave-resource-core refine interfaces

--- weave-core/weave-resource-core/src/main/java/io/aftersound/weave/resource/ResourceConfig.java
+++ weave-core/weave-resource-core/src/main/java/io/aftersound/weave/resource/ResourceConfig.java
@@ -3,5 +3,10 @@ package io.aftersound.weave.resource;
 import io.aftersound.weave.metadata.Control;
 
 public interface ResourceConfig extends Control {
-    String getName();
+
+    /**
+     * @return name of resource if pairing {@link ResourceManager} successfully creates one
+     *         based on this resource config
+     */
+    String getResourceName();
 }
