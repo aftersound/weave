5465a54c347340110bb3c36c3b7d4cb68226a3ba
aftersound <aftersound@gmail.com>
2020-05-03 17:14:51 -0700

    weave-config KeyFilters common helper methods

--- weave-shared/weave-config/src/main/java/io/aftersound/weave/config/KeyFilters.java
+++ weave-shared/weave-config/src/main/java/io/aftersound/weave/config/KeyFilters.java
@@ -2,20 +2,28 @@ package io.aftersound.weave.config;
 
 public final class KeyFilters {
 
-    public static final KeyFilter SECURITY_KEY = new KeyFilter() {
+    public static KeyFilter keyWithTag(final String tag) {
+        return new KeyFilter() {
 
-        @Override
-        public boolean isAcceptable(Key<?> key) {
-            return key.hasTag(Tags.SECURITY);
-        }
-    };
+            @Override
+            public boolean isAcceptable(Key<?> key) {
+                return key.hasTag(tag);
+            }
+        };
+    }
 
-    public static final KeyFilter NOT_SECURITY_KEY = new KeyFilter() {
+    public static KeyFilter keyWithoutTag(final String tag) {
+        return new KeyFilter() {
 
-        @Override
-        public boolean isAcceptable(Key<?> key) {
-            return !key.hasTag(Tags.SECURITY);
-        }
-    };
+            @Override
+            public boolean isAcceptable(Key<?> key) {
+                return !key.hasTag(tag);
+            }
+        };
+    }
+
+    public static final KeyFilter SECURITY_KEY = keyWithTag(Tags.SECURITY);
+
+    public static final KeyFilter NOT_SECURITY_KEY = keyWithoutTag(Tags.SECURITY);
 
 }
