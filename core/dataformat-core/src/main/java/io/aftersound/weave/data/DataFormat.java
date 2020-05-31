5bb73106ee637969f8cc2a8ceaca584eac9c4145
aftersound <aftersound@gmail.com>
2020-05-30 19:49:02 -0700

    deprecate dataformat-core

--- core/dataformat-core/src/main/java/io/aftersound/weave/data/DataFormat.java
+++
@@ -1,7 +0,0 @@
-package io.aftersound.weave.data;
-
-public interface DataFormat {
-    String getType();
-    Serializer serializer();
-    Deserializer deserializer();
-}
--- core/dataformat-core/src/main/java/io/aftersound/weave/data/DataFormatControl.java
+++
@@ -1,6 +0,0 @@
-package io.aftersound.weave.data;
-
-import io.aftersound.weave.metadata.Control;
-
-public interface DataFormatControl extends Control {
-}
