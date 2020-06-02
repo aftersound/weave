1694c56fb0ad648d92d4a39f8c4709aaa6376b1b
aftersound <aftersound@gmail.com>
2020-06-02 16:48:56 -0700

    rename to Triplet

--- shared/misc/src/main/java/io/aftersound/weave/utils/Triplet.java
+++ shared/misc/src/main/java/io/aftersound/weave/utils/Triplet.java
@@ -1,19 +1,19 @@
 package io.aftersound.weave.utils;
 
-public class Triple<F,S,T> {
+public class Triplet<F,S,T> {
 
     private final F first;
     private final S second;
     private final T third;
 
-    private Triple(F first, S second, T third) {
+    private Triplet(F first, S second, T third) {
         this.first = first;
         this.second = second;
         this.third = third;
     }
 
-    public static <F,S,T> Triple<F,S,T> of(F first, S second, T third) {
-        return new Triple(first, second, third);
+    public static <F,S,T> Triplet<F,S,T> of(F first, S second, T third) {
+        return new Triplet(first, second, third);
     }
 
     public F first() {
