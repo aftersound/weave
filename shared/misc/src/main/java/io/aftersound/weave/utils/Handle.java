0bb15c2b5f0a212dfe87b1d1e4eefcf3c87d3d95
aftersound <aftersound@gmail.com>
2020-06-01 23:24:36 -0700

    make Handle support sington and multi-ton

--- shared/misc/src/main/java/io/aftersound/weave/utils/Handle.java
+++ shared/misc/src/main/java/io/aftersound/weave/utils/Handle.java
@@ -3,31 +3,55 @@ package io.aftersound.weave.utils;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A handle which holds object of singleton or multi-ton and provides access to it.
+ * @param <T>
+ *          generic type of objects being handled
+ */
 public class Handle<T> {
 
-    private static final Map<Class<?>, Handle<?>> INSTANCES = new HashMap<>();
+    private static final Map<String, Handle<?>> INSTANCES = new HashMap<>();
+    private static final Object INSTANCE_LOCK = new Object();
 
+    private final String fullIdentifier;
     private final Class<T> type;
+
     private T obj;
 
     private boolean locked;
 
-    private static final Object INSTANCE_LOCK = new Object();
-
-    private Handle(Class<T> type) {
+    private Handle(String fullIdentifier, Class<T> type) {
+        this.fullIdentifier = fullIdentifier;
         this.type = type;
     }
 
+    /**
+     * return existing handle or create a handle of given type and id
+     * @param id
+     *          identifier, uniquely identify a handle together with type
+     * @param type
+     *          type of object to be handled
+     * @param <T>
+     *          generic type of object to be handled
+     * @return
+     *          a handle associated with given id and type
+     */
     @SuppressWarnings("unchecked")
-    public static <T> Handle<T> of(Class<T> type) {
+    public static <T> Handle<T> of(String id, Class<T> type) {
         synchronized (INSTANCE_LOCK) {
-            if (!INSTANCES.containsKey(type)) {
-                INSTANCES.put(type, new Handle<>(type));
+            String fullIdentifier = type.getName() + "_" + id;
+            if (!INSTANCES.containsKey(fullIdentifier)) {
+                INSTANCES.put(fullIdentifier, new Handle<>(fullIdentifier, type));
             }
         }
         return (Handle<T>) INSTANCES.get(type);
     }
 
+    /**
+     * Bind the object once and for all
+     * @param obj
+     *          the object being handled
+     */
     public synchronized void setAndLock(T obj) {
         if (!locked) {
             this.obj = obj;
@@ -35,15 +59,24 @@ public class Handle<T> {
         this.locked = true;
     }
 
+    /**
+     * @return
+     *          the object being handled
+     */
     public synchronized T get() {
         return obj;
     }
 
-    public synchronized T remove() {
+    /**
+     * detach this handle from the object
+     * @return
+     *          the object
+     */
+    public synchronized T detach() {
         T removed = obj;
 
         // remove handle
-        INSTANCES.remove(type);
+        INSTANCES.remove(fullIdentifier);
         obj = null;
 
         return obj;
