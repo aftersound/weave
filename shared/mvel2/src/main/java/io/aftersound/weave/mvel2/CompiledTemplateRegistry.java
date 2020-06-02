fe3610ddcac1c0c179dfa571e1e5fc0eae901a34
aftersound <aftersound@gmail.com>
2020-06-02 16:49:08 -0700

    add comment to mvel2 module

--- shared/mvel2/src/main/java/io/aftersound/weave/mvel2/CompiledTemplateRegistry.java
+++ shared/mvel2/src/main/java/io/aftersound/weave/mvel2/CompiledTemplateRegistry.java
@@ -8,8 +8,23 @@ import org.mvel2.templates.TemplateCompiler;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * A {@link Registry} which
+ *  1.compiles raw MVEL2 string template
+ *  2.caches {@link CompiledTemplate}
+ *  3.makes {@link CompiledTemplate} available when raw template is given
+ */
 public final class CompiledTemplateRegistry extends Registry<String, CompiledTemplate> {
 
+    /**
+     * Get {@link CompiledTemplate} which is compiled from raw template.
+     *   1.If exists, simply return cached one
+     *   2.If not exists, create one, cache it then return
+     * @param rawTemplate
+     *          raw MVEL2 string template
+     * @return
+     *          {@link CompiledTemplate} which is compiled from raw template.
+     */
     public CompiledTemplate getCompiledTemplate(String rawTemplate) {
         return get(
                 rawTemplate,
@@ -24,6 +39,15 @@ public final class CompiledTemplateRegistry extends Registry<String, CompiledTem
         );
     }
 
+    /**
+     * Get {@link CompiledTemplates} which holds all {@link CompiledTemplate}s compiled from raw templates.
+     *   1.If exists, simply return cached one
+     *   2.If not exists, create one, cache it then return
+     * @param rawTemplates
+     *          a list of raw MVEL2 string template
+     * @return
+     *          {@link CompiledTemplates} which holds all {@link CompiledTemplate}s compiled from raw templates.
+     */
     public CompiledTemplates getCompiledTemplates(List<String> rawTemplates) {
         Map<String, CompiledTemplate> compiledTemplate = get(
                 rawTemplates,
--- shared/mvel2/src/main/java/io/aftersound/weave/mvel2/CompiledTemplates.java
+++ shared/mvel2/src/main/java/io/aftersound/weave/mvel2/CompiledTemplates.java
@@ -6,8 +6,18 @@ import org.mvel2.templates.CompiledTemplate;
 import java.util.Collections;
 import java.util.Map;
 
+/**
+ * A wrapper of {@link Map} between raw MVEL2 string template and {@link CompiledTemplate},
+ * which provides easy access to {@link CompiledTemplate}
+ */
 public class CompiledTemplates {
 
+    /**
+     * Default instance of {@link CompiledTemplateRegistry}, which manages
+     * {@link CompiledTemplates} in according to raw MVEL2 string template.
+     * Application can still create instances of {@link CompiledTemplateRegistry}
+     * if DefaultInstance cannot fulfill the need.
+     */
     public static final Handle<CompiledTemplateRegistry> REGISTRY = Handle.of(
             "DefaultInstance",
             CompiledTemplateRegistry.class
@@ -15,7 +25,12 @@ public class CompiledTemplates {
 
     private final Map<String, CompiledTemplate> compiledTemplates;
 
-    public CompiledTemplates(Map<String, CompiledTemplate> compiledTemplates) {
+    /**
+     * Make the constructor only available to {@link CompiledTemplateRegistry}
+     * @param compiledTemplates
+     *          a map of raw MVEL2 string template and {@link CompiledTemplate}
+     */
+    CompiledTemplates(Map<String, CompiledTemplate> compiledTemplates) {
         if (compiledTemplates != null) {
             this.compiledTemplates = Collections.unmodifiableMap(compiledTemplates);
         } else {
@@ -23,6 +38,13 @@ public class CompiledTemplates {
         }
     }
 
+    /**
+     * get {@link CompiledTemplate} compiled from raw template
+     * @param rawTemplate
+     *          a raw MVEL2 string template
+     * @return
+     *          {@link CompiledTemplate} compiled from raw template
+     */
     public CompiledTemplate get(String rawTemplate) {
         return compiledTemplates.get(rawTemplate);
     }
