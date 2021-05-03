58699e45da986decf6841decb44c4ccafccf439d
aftersound <aftersound@gmail.com>
2021-05-02 21:39:45 -0700

    implement PipelineFactory

--- core/process-core/src/main/java/io/aftersound/weave/pipeline/PipelineFactory.java
+++ core/process-core/src/main/java/io/aftersound/weave/pipeline/PipelineFactory.java
@@ -1,88 +1,62 @@
 package io.aftersound.weave.pipeline;
 
-import io.aftersound.weave.component.ComponentConfig;
-import io.aftersound.weave.component.ComponentRegistry;
+import io.aftersound.weave.actor.ActorRegistry;
+import io.aftersound.weave.component.ComponentRepository;
 import io.aftersound.weave.process.Processor;
-import io.aftersound.weave.utils.Pair;
+import io.aftersound.weave.process.ProcessorFactory;
+import io.aftersound.weave.utils.ExprTreeParsingException;
 import io.aftersound.weave.utils.TextualExprTreeParser;
 import io.aftersound.weave.utils.TreeNode;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PipelineFactory {
 
-    private final ComponentRegistry componentRegistry;
-    public PipelineFactory(ComponentRegistry componentRegistry) {
-        this.componentRegistry = componentRegistry;
+    private final ComponentRepository componentRepository;
+
+    public PipelineFactory(ComponentRepository componentRepository) {
+        this.componentRepository = componentRepository;
     }
 
-    public Pipeline createPipeline(String spec, Map<String, String> options) {
+    public Pipeline createPipeline(TreeNode pipelineSpec, Map<String, String> options) {
         try {
-            TreeNode specTreeNode = TextualExprTreeParser.parse(spec);
-            if (!"Pipeline".equals(specTreeNode.getData())) {
-                throw new Exception();
-            }
+            assert (pipelineSpec != null);
 
             List<Processor> processorList = new ArrayList<>();
-            for (TreeNode procSpec : specTreeNode.getChildren()) {
-                Processor processor = createProcessor(procSpec, options);
+            for (TreeNode processSpec : pipelineSpec.getChildren()) {
+                Processor processor = createProcessor(processSpec, options);
                 processorList.add(processor);
             }
             return new Pipeline(processorList);
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new IllegalArgumentException("Exception occurred on creating Pipeline", e);
         }
-        return null;
+    }
+
+    public Pipeline createPipeline(String specExpr, Map<String, String> options) {
+        TreeNode pipelineSpec;
+        try {
+            pipelineSpec = TextualExprTreeParser.parse(specExpr);
+        } catch (ExprTreeParsingException e) {
+            throw new IllegalArgumentException("Exception occurred on creating Pipeline", e);
+        }
+        return createPipeline(pipelineSpec, options);
     }
 
     /**
      * Create a {@link Processor} which honors given specification and options
-     * @param procSpec
-     *          specification in form of {@link TreeNode}, or in textual expression form: NAMESPACE:OPERATION(PARAMETER_GROUP)
-     * @param options
-     *          options which contains configuration for the {@link Processor}
-     * @return
-     *          a {@link Processor}
-     * @throws Exception
-     *          any exception
+     *
+     * @param processSpec specification in form of {@link TreeNode}, or in textual expression form: [NAMESPACE:]OPERATION(PARAMETER_GROUP)
+     * @param options     options which contains configuration for the {@link Processor}
+     * @return a {@link Processor}
      */
-    private Processor createProcessor(TreeNode procSpec, Map<String, String> options) throws Exception {
-        Pair<String, String> nsAndOp = parseNamespaceAndOperation(procSpec.getData());
-        String type = nsAndOp.first();
-        String op = nsAndOp.second();
-        String paramGroup = procSpec.getChildren().get(0).getData();
-        String processorId = type + "." + op + "." + paramGroup;
-
-//        ComponentConfig processorConfig = new ComponentConfig();
-//        processorConfig.setType(type);
-//        processorConfig.setId(processorId);
-//        processorConfig.setOptions(getProcessorOptions(op, paramGroup, options));
-//
-//        componentRegistry.initializeComponent(processorConfig);
-//        return componentRegistry.getComponent(processorId);
-        // TODO
-        return null;
-    }
-
-    private Pair<String, String> parseNamespaceAndOperation(String opStr) {
-        int pos = opStr.indexOf(':');
-        String ns = opStr.substring(0, pos);
-        String op = opStr.substring(pos + 1);
-        return Pair.of(ns, op);
-    }
-
-    private Map<String, String> getProcessorOptions(String op, String paramGroup, Map<String, String> options) {
-        final String prefix = paramGroup + ":";
-        Map<String, String> subOptions = new HashMap<>();
-        for (Map.Entry<String, String> e : options.entrySet()) {
-            if (e.getKey().startsWith(prefix)) {
-                subOptions.put(e.getKey().substring(prefix.length()), e.getValue());
-            }
-        }
-        return subOptions;
+    private Processor createProcessor(TreeNode processSpec, Map<String, String> options) {
+        String name = processSpec.getData();
+        String paramGroup = processSpec.getDataOfChildAt(0);
+        ActorRegistry<ProcessorFactory> pfr = componentRepository.getComponent("processor.factory.registry");
+        return pfr.get(name).create(paramGroup, options);
     }
 
 }
