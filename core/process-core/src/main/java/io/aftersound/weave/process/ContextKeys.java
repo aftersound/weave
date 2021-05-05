a4151b477c757ca969d0dee2db5e76b8e2e7f0bc
aftersound <aftersound@gmail.com>
2021-05-04 18:26:21 -0700

    add more common context object keys

--- core/process-core/src/main/java/io/aftersound/weave/process/ContextKeys.java
+++ core/process-core/src/main/java/io/aftersound/weave/process/ContextKeys.java
@@ -11,13 +11,23 @@ import java.util.Map;
 public final class ContextKeys {
 
     /**
-     * Key of common context object which holds variables
-     */
-    public static final Key<Map<String, Object>> VARIABLES = Key.of("Variables");
-
-    /**
      * Key of common context object which is an instance of {@link ComponentRepository}
      */
     public static final Key<ComponentRepository> COMPONENT_REPOSITORY = Key.of("ComponentRepository");
+
+    /**
+     * Key of common context object which holds variables
+     */
+    public static final Key<Map<String, Object>> VARIABLES = Key.of("variables");
+
+    /**
+     * Key of knob which controls whether observer of processor is enabled
+     */
+    public static final Key<Boolean> OBSERVER_ENABLED = Key.of("observer.enabled");
+
+    /**
+     * Key of common context object which holds what's observed by observers
+     */
+    public static final Key<Boolean> OBSERVED = Key.of("observer.enabled");
 
 }
