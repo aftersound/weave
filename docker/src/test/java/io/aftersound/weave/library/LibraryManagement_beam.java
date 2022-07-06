6401bb97c250837dee6375da8d767dd805f2f059
jaloid <aftersound@gmail.com>
2022-07-05 21:42:55 -0700

    docker source for apache beam 2.38.0 with flink runner targeting flink 1.11.6

--- docker/src/test/java/io/aftersound/weave/library/LibraryManagement_beam.java
+++ docker/src/test/java/io/aftersound/weave/library/LibraryManagement_beam.java
@@ -7,9 +7,17 @@ import static io.aftersound.weave.library.Target.Docker;
 public class LibraryManagement_beam {
 
     @Test
+    public void b2380_f1116() throws Exception {
+        // prepare libraries needed for docker image
+        //   Apache Beam 2.38.0 with beam-runner-flink-1.11.6
+        //   Apache Flink client sdk for 1.13.6 and required dependencies
+        new LibraryManagement("b2380_f1116").executeFor(Docker);
+    }
+
+    @Test
     public void b2380_f1136() throws Exception {
         // prepare libraries needed for docker image
-        //   Apache Beam 2.38.0 with beam-runner-flink-1.13
+        //   Apache Beam 2.38.0 with beam-runner-flink-1.13.6
         //   Apache Flink client sdk for 1.13.6 and required dependencies
         new LibraryManagement("b2380_f1136").executeFor(Docker);
     }
