6910e502a669e969a017b0cb43290dbc4c67a017
jaloid <aftersound@gmail.com>
2022-07-05 22:08:21 -0700

    source for docker image supporting Apache Beam 2.38.0 with flink runner targeting flink 1.12.7

---
+++ docker/b2380_f1127/docker_build.sh
@@ -0,0 +1,1 @@
+docker build . -t aftersound/weave:b2380_f1127-0.0.1-SNAPSHOT
--- docker/src/test/java/io/aftersound/weave/library/LibraryManagement_beam.java
+++ docker/src/test/java/io/aftersound/weave/library/LibraryManagement_beam.java
@@ -8,17 +8,31 @@ public class LibraryManagement_beam {
 
     @Test
     public void b2380_f1116() throws Exception {
-        // prepare libraries needed for docker image
-        //   Apache Beam 2.38.0 with beam-runner-flink-1.11.6
-        //   Apache Flink client sdk for 1.13.6 and required dependencies
+        // prepare libraries needed for docker image which supports
+        // submitting flink job to Apache Flink cluster on 1.11.6 via Apache Beam
+        //   1.Apache Beam 2.38.0 core and its required dependencies
+        //   2.beam-runners-flink-1.11 and its required dependencies
+        //   3.Apache Flink client sdk for 1.11.6 and its required dependencies
         new LibraryManagement("b2380_f1116").executeFor(Docker);
     }
 
     @Test
+    public void b2380_f1127() throws Exception {
+        // prepare libraries needed for docker image which supports
+        // submitting flink job to Apache Flink cluster on 1.12.7 via Apache Beam
+        //   1.Apache Beam 2.38.0 core and its required dependencies
+        //   2.beam-runners-flink-1.12 and its required dependencies
+        //   3.Apache Flink client sdk for 1.12.7 and its required dependencies
+        new LibraryManagement("b2380_f1127").executeFor(Docker);
+    }
+
+    @Test
     public void b2380_f1136() throws Exception {
-        // prepare libraries needed for docker image
-        //   Apache Beam 2.38.0 with beam-runner-flink-1.13.6
-        //   Apache Flink client sdk for 1.13.6 and required dependencies
+        // prepare libraries needed for docker image which supports
+        // submitting flink job to Apache Flink cluster on 1.13.6 via Apache Beam
+        //   1.Apache Beam 2.38.0 core and its required dependencies
+        //   2.beam-runners-flink-1.13 and its required dependencies
+        //   3.Apache Flink client sdk for 1.13.6 and its required dependencies
         new LibraryManagement("b2380_f1136").executeFor(Docker);
     }
 
