4ca1f86c2e284d9f5b1bf7265e77546503bdfd52
jaloid <aftersound@gmail.com>
2022-07-08 16:51:11 -0700

    make it a bit tolerable to errors  on value func descriptors handling

--- core/schema-core/src/main/java/io/aftersound/weave/common/MasterValueFuncFactory.java
+++ core/schema-core/src/main/java/io/aftersound/weave/common/MasterValueFuncFactory.java
@@ -1,22 +1,28 @@
 package io.aftersound.weave.common;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.aftersound.weave.common.valuefunc.Descriptor;
 import io.aftersound.weave.utils.TextualExprTreeParser;
 import io.aftersound.weave.utils.TreeNode;
 
-import java.io.InputStream;
 import java.util.*;
 
+/**
+ * The master value func factory which creates {@link ValueFunc}
+ * based on given specification
+ */
 public final class MasterValueFuncFactory {
-
-    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     private static Object lock = new Object();
     private static List<ValueFuncFactory> subordinates = Collections.emptyList();
     private static Map<String, Descriptor> valueFuncDescriptorByName = Collections.emptyMap();
     private static Map<String, Descriptor> valueFuncDescriptorByAlias = Collections.emptyMap();
 
+    /**
+     * Instantiate given classes of {@link ValueFuncFactory} and leverage these instances as subordinates
+     *
+     * @param valueFuncFactoryClasses full qualified names of {@link ValueFuncFactory} implementation classes
+     * @throws Exception exception needs attention from caller
+     */
     public static void init(String... valueFuncFactoryClasses) throws Exception {
         synchronized (lock) {
             final List<ValueFuncFactory> vffList = new ArrayList<>();
@@ -30,24 +36,14 @@ public final class MasterValueFuncFactory {
                 vffList.add(valueFuncFactory);
 
                 // identify ValueFunc Descriptor(s) managed by the ValueFuncFactory
-                try (InputStream is = cls.getResourceAsStream("/" + cls.getSimpleName() + ".json")) {
-                    Descriptor[] descriptors = MAPPER.readValue(is, Descriptor[].class);
-                    vfdList.addAll(Arrays.asList(descriptors));
-                } catch (Exception e) {
-                    throw new IllegalArgumentException(cls.getSimpleName() + ".json is either missing or malformed", e);
-                }
+                vfdList.addAll(ValueFuncDescriptorHelper.getDescriptors(cls));
             }
 
             subordinates = Collections.unmodifiableList(vffList);
 
             Collections.sort(
                     vfdList,
-                    new Comparator<Descriptor>() {
-                        @Override
-                        public int compare(Descriptor d1, Descriptor d2) {
-                            return d1.getName().compareTo(d2.getName());
-                        }
-                    }
+                    Comparator.comparing(Descriptor::getName)
             );
             Map<String, Descriptor> vfdByName = new LinkedHashMap<>(vfdList.size());
             Map<String, Descriptor> vfdByAlias = new LinkedHashMap<>(vfdList.size());
@@ -66,10 +62,21 @@ public final class MasterValueFuncFactory {
         }
     }
 
+    /**
+     * Get all the {@link Descriptor}s of {@link ValueFunc}s currently under awareness
+     *
+     * @return Get all the {@link Descriptor}s of {@link ValueFunc}s currently under awareness
+     */
     public static Map<String, Descriptor> getManagedValueFuncDescriptors() {
         return valueFuncDescriptorByName;
     }
 
+    /**
+     * Get the {@link Descriptor} of target {@link ValueFunc} with specified name
+     *
+     * @param valueFuncName the name of target {@link ValueFunc}
+     * @return the {@link Descriptor} of target {@link ValueFunc} with specified name
+     */
     public static Descriptor getManagedValueFuncDescriptor(String valueFuncName) {
         Descriptor descriptor = valueFuncDescriptorByName.get(valueFuncName);
         if (descriptor != null) {
@@ -79,6 +86,14 @@ public final class MasterValueFuncFactory {
         return valueFuncDescriptorByAlias.get(valueFuncName);
     }
 
+    /**
+     * Create {@link ValueFunc} which conforms to given spec in form of {@link TreeNode}
+     *
+     * @param spec the specification for desired {@link ValueFunc}
+     * @param <S>  the type of input accepted by desired {@link ValueFunc} in generic form
+     * @param <T>  the type of output produced by desired {@link ValueFunc} in generic form
+     * @return a {@link ValueFunc} which conforms to given spec
+     */
     public static <S, T> ValueFunc<S, T> create(TreeNode spec) {
         ValueFunc<?, ?> valueFunc = null;
         for (ValueFuncFactory subordinate : subordinates) {
@@ -95,6 +110,14 @@ public final class MasterValueFuncFactory {
         throw new IllegalArgumentException(spec.toString() + " is not supported");
     }
 
+    /**
+     * Create {@link ValueFunc} which conforms to given spec in form of string
+     *
+     * @param spec the specification for desired {@link ValueFunc}
+     * @param <S>  the type of input accepted by desired {@link ValueFunc} in generic form
+     * @param <T>  the type of output produced by desired {@link ValueFunc} in generic form
+     * @return a {@link ValueFunc} which conforms to given spec
+     */
     public static <S, T> ValueFunc<S, T> create(String spec) {
         TreeNode specAsTreeNode;
         try {
---
+++ core/schema-core/src/main/java/io/aftersound/weave/common/ValueFuncDescriptorHelper.java
@@ -0,0 +1,45 @@
+package io.aftersound.weave.common;
+
+import io.aftersound.weave.common.valuefunc.Descriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class ValueFuncDescriptorHelper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ValueFuncDescriptorHelper.class);
+
+    private static Object mapper;
+
+    public static List<Descriptor> getDescriptors(Class<? extends ValueFuncFactory> valueFuncFactoryClass) {
+        if (valueFuncFactoryClass == null) {
+            return Collections.emptyList();
+        }
+
+        final String jsonResource = "/" + valueFuncFactoryClass.getSimpleName() + ".json";
+
+        try (InputStream is = valueFuncFactoryClass.getResourceAsStream(jsonResource)) {
+            com.fasterxml.jackson.databind.ObjectMapper objectMapper = (com.fasterxml.jackson.databind.ObjectMapper) getMapper();
+            Descriptor[] descriptors = objectMapper.readValue(is, Descriptor[].class);
+            return Arrays.asList(descriptors);
+        } catch (IOException e) {
+            LOGGER.error("Failed to get resource at class path '{}'", jsonResource, e);
+            return Collections.emptyList();
+        } catch (Throwable t) {
+            LOGGER.error("Failed to read Descriptor(s) from resource at class path '{}'", jsonResource, t);
+            return Collections.emptyList();
+        }
+    }
+
+    private static Object getMapper() {
+        if (mapper == null) {
+            mapper = new com.fasterxml.jackson.databind.ObjectMapper();
+        }
+        return mapper;
+    }
+}
