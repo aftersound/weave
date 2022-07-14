225c7a6a939e31c118e26b85396f62966443e8d8
jaloid <aftersound@gmail.com>
2022-07-13 22:13:44 -0700

    introduce ChainOfResponsibilityValueFunc

---
+++ core/schema-core/src/main/java/io/aftersound/weave/common/ChainOfResponsibilityValueFunc.java
@@ -0,0 +1,30 @@
+package io.aftersound.weave.common;
+
+import java.util.List;
+
+/**
+ * A {@link ValueFunc} has chain-of-responsibility behavior
+ * @param <S> source/input type
+ * @param <T> target/output type
+ */
+public class ChainOfResponsibilityValueFunc<S, T> implements ValueFunc<S, T> {
+
+    private final List<ValueFunc<S, T>> subordinates;
+
+    public ChainOfResponsibilityValueFunc(List<ValueFunc<S, T>> subordinates) {
+        this.subordinates = subordinates;
+    }
+
+    @Override
+    public T apply(S source) {
+        T v = null;
+        for (ValueFunc<S, T> subordinate : subordinates) {
+            v = subordinate.apply(source);
+            if (v != null) {
+                break;
+            }
+        }
+        return v;
+    }
+
+}
---
+++ core/schema-core/src/test/java/io/aftersound/weave/common/ChainOfResponsibilityValueFuncTest.java
@@ -0,0 +1,44 @@
+package io.aftersound.weave.common;
+
+import io.aftersound.weave.utils.MapBuilder;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class ChainOfResponsibilityValueFuncTest {
+
+    @Test
+    public void apply() {
+        ValueFunc<Map<String, String>, String> cor = new ChainOfResponsibilityValueFunc<>(
+                Arrays.asList(
+                        new MapGetFunc("k1"),
+                        new MapGetFunc("k2"),
+                        new MapGetFunc("k3"),
+                        new MapGetFunc("k4")
+                )
+        );
+        String v = cor.apply(MapBuilder.hashMap().kv("k3", "v3").build());
+        assertEquals("v3", v);
+
+        v = cor.apply(MapBuilder.hashMap().kv("k5", "v5").build());
+        assertNull(v);
+    }
+
+    private static class MapGetFunc implements ValueFunc<Map<String, String>, String> {
+
+        private final String key;
+
+        public MapGetFunc(String key) {
+            this.key = key;
+        }
+
+        @Override
+        public String apply(Map<String, String> source) {
+            return source != null ? source.get(key) : null;
+        }
+    }
+
+}
