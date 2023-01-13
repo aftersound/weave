b4e2768f625aad52fe242e3a5bf3079cc2e884aa
jaloid <aftersound@gmail.com>
2023-01-12 20:15:52 -0800

    rename library-management to library-core

---
+++ core/library-core/src/main/java/io/aftersound/weave/common/LibraryInfo.java
@@ -0,0 +1,57 @@
+package io.aftersound.weave.common;
+
+import java.util.Set;
+
+public class LibraryInfo {
+
+    private String groupId;
+    private String artifactId;
+    private String version;
+    private String location;
+    private Set<String> tags;
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+
+    public boolean hasTag(String tag) {
+        return tags != null ? tags.contains(tag) : false;
+    }
+
+}
