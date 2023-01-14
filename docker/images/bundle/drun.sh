45b9a4527369ae460dd2b8254b9ee4fad8810f64
jaloid <aftersound@gmail.com>
2023-01-13 22:24:04 -0800

    docker updates

--- docker/images/bundle/drun.sh
+++ docker/images/bundle/drun.sh
@@ -1,3 +1,4 @@
 #docker container run -it -p 8080:8080 --env APPLICATION=service.demo aftersound/weave:bundle --name service.demo
-port=$1
-docker container run -it -p $port:8080 --env APPLICATION=service.demo aftersound/weave:bundle --name service.demo
+namespace=$1
+port=$2
+docker container run -it -p $port:8080 --env APPLICATION=service.$namespace aftersound/weave:bundle --name service.$namespace
