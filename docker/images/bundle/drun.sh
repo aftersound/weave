f789eca4834d48f40f4d316f7f6dfb908d481dbf
aftersound <aftersound@gmail.com>
2023-02-17 21:30:41 -0800

    rename application properties files

--- docker/images/bundle/drun.sh
+++ docker/images/bundle/drun.sh
@@ -1,4 +1,6 @@
 #docker container run -it -p 8080:8080 --env APPLICATION=service.demo aftersound/weave:bundle --name service.demo
-namespace=$1
+app=$1
 port=$2
-docker container run -it -p $port:8080 --env APPLICATION=service.$namespace aftersound/weave:bundle --name service.$namespace
+rcm_server=$3
+echo docker container run -it -p $port:8080 --env WEAVE_APP=$app --env WEAVE_RUNTIME_CONFIG_SERVER=$rcm_server aftersound/weave:bundle --name $app
+docker container run -it -p $port:8080 --env WEAVE_APP=$app --env WEAVE_RUNTIME_CONFIG_SERVER=$rcm_server aftersound/weave:bundle --name $app
