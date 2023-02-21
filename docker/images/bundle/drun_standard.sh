aa283a01d2a716d2c231da399a52f71eef8886de
jaloid <aftersound@gmail.com>
2023-02-21 15:38:49 -0800

    update docker image 'bundle' content
    1.update demo instruction and its service uri
    2.update standard profile application properties and docker run script to use WEAVE_RCS

--- docker/images/bundle/drun_standard.sh
+++ docker/images/bundle/drun_standard.sh
@@ -2,6 +2,6 @@
 ns=$1
 app=$2
 port=$3
-rcm_server=$4
-echo docker container run --name $app -it -p $port:8080 --env WEAVE_PRF=standard --env WEAVE_APP=$app --env WEAVE_RUNTIME_CONFIG_SERVER=$rcm_server aftersound/weave:bundle
-docker container run -it -p $port:8080 --env WEAVE_PRF=standard --env WEAVE_NS=$ns --env WEAVE_APP=$app --env WEAVE_RUNTIME_CONFIG_SERVER=$rcm_server aftersound/weave:bundle
+rcs=$4
+echo docker container run --name $app -it -p $port:8080 --env WEAVE_PRF=standard --env WEAVE_APP=$app --env WEAVE_RCS=$rcm_server aftersound/weave:bundle
+docker container run -it -p $port:8080 --env WEAVE_PRF=standard --env WEAVE_NS=$ns --env WEAVE_APP=$app --env WEAVE_RCS=$rcs aftersound/weave:bundle
