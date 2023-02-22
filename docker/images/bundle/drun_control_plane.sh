eee7e5649e1af643b66df17ac2bdb421c21c1bf7
jaloid <aftersound@gmail.com>
2023-02-22 09:21:10 -0800

    control plane to management plane

--- docker/images/bundle/drun_control_plane.sh
+++
@@ -1,6 +0,0 @@
-#docker container run -it -p 8080:8080 --env APPLICATION=service.demo aftersound/weave:bundle --name service.demo
-ns=$1
-app=$2
-port=$3
-echo docker container run --name $app -it -p $port:8080 --env WEAVE_PRF=control-plane --env WEAVE_APP=$app aftersound/weave:bundle
-docker container run -it -p $port:8080 --env WEAVE_PRF=control-plane --env WEAVE_NS=$ns --env WEAVE_APP=$app aftersound/weave:bundle
