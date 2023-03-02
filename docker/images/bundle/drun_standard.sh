dfeba1b5f956ec536c9d502114216d42c646746f
aftersound <aftersound@gmail.com>
2023-03-02 12:27:20 -0800

    update content of docker image 'bundle'

--- docker/images/bundle/drun_standard.sh
+++ docker/images/bundle/drun_standard.sh
@@ -2,6 +2,7 @@
 ns=$1
 app=$2
 port=$3
-rcs=$4
-echo docker container run --name $app -it -p $port:8080 --env WEAVE_PRF=standard --env WEAVE_APP=$app --env WEAVE_RCS=$rcm_server aftersound/weave:bundle
-docker container run -it -p $port:8080 --env WEAVE_PRF=standard --env WEAVE_NS=$ns --env WEAVE_APP=$app --env WEAVE_RCS=$rcs aftersound/weave:bundle
+mgr=$4
+ena=$5
+echo docker container run --name $app -it -p $port:8080 --env WEAVE_PRF=standard --env WEAVE_APP=$app --env WEAVE_MGR=$mgr --env WEAVE_ENA=$ena aftersound/weave:bundle
+docker container run -it -p $port:8080 --env WEAVE_PRF=standard --env WEAVE_NS=$ns --env WEAVE_APP=$app --env WEAVE_MGR=$mgr --env WEAVE_ENA=$ena aftersound/weave:bundle
