d460e28701f80eea97c380006bc54fa6987c89bb
aftersound <aftersound@gmail.com>
2023-03-09 21:07:59 -0800

    placeholder methods in JobManager

--- core/job-core/src/main/java/io/aftersound/weave/job/JobManager.java
+++ core/job-core/src/main/java/io/aftersound/weave/job/JobManager.java
@@ -1,4 +1,27 @@
 package io.aftersound.weave.job;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+import javax.sql.DataSource;
+import java.util.UUID;
+
 public class JobManager {
+
+    private final DataSource dataSource;
+    private final String table;
+
+    public JobManager(DataSource dataSource, String table) {
+        this.dataSource = dataSource;
+        this.table = table;
+    }
+
+    public String createJob(JsonNode jobRequest) {
+        // TODO
+        return UUID.randomUUID().toString();
+    }
+
+    public String getJobStatus(String jobId) {
+        return "unknown";
+    }
+
 }
