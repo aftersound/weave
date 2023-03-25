81bb7a419f21f4866e686edb7221202cf900bb2d
aftersound <aftersound@gmail.com>
2023-03-24 22:36:01 -0700

    add Status.java representing job status code

---
+++ core/job-core/src/main/java/io/aftersound/weave/job/Status.java
@@ -0,0 +1,14 @@
+package io.aftersound.weave.job;
+
+/**
+ * Job Status Code
+ */
+public enum Status {
+    CREATED,    // job is newly created
+    ASSIGNED,   // job is assigned with a runner is assigned to run the job
+    ACCEPTED,   // job is accepted by the assigned runner
+    RUNNING,    // job is running
+    FINISHED,   // job is finished
+    FAILED,     // job is failed
+    KILLED      // job is killed or cancelled or terminated
+}
