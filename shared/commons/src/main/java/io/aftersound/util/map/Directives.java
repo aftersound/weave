050992e1ec2e0c051606e72278af7a839e519256
jaloid <aftersound@gmail.com>
2025-01-20 10:59:48 -0800

    fix compilation error

--- shared/commons/src/main/java/io/aftersound/util/map/Directives.java
+++ shared/commons/src/main/java/io/aftersound/util/map/Directives.java
@@ -2,17 +2,14 @@ package io.aftersound.util.map;
 
 import io.aftersound.util.AttributeHolder;
 
-public class Directives extends AttributeHolder<Directives> {
+public class Directives extends AttributeHolder {
 
     public Directives() {
         super();
     }
 
     public Directives acquire(Directives others) {
-        if (others != null) {
-            super.acquire(others);
-        }
-        return this;
+        return super.acquire(others);
     }
 
 }
