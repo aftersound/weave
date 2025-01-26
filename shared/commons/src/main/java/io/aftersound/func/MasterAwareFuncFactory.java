ee19a7e44d535ad744e83ea4381aa3a98731ef90
jaloid <aftersound@gmail.com>
2025-01-25 20:27:25 -0800

    comment on MasterAwareFactory

--- shared/commons/src/main/java/io/aftersound/func/MasterAwareFuncFactory.java
+++ shared/commons/src/main/java/io/aftersound/func/MasterAwareFuncFactory.java
@@ -1,10 +1,19 @@
 package io.aftersound.func;
 
+/**
+ * Any {@link FuncFactory} which works as subordinate under a {@link MasterFuncFactory}
+ * must extend this
+ */
 public abstract class MasterAwareFuncFactory implements FuncFactory {
 
     protected FuncFactory masterFuncFactory;
 
-    public void setMasterFuncFactory(FuncFactory masterFuncFactory) {
+    /**
+     * Caller will make this aware of master {@link FuncFactory}
+     *
+     * @param masterFuncFactory - the master func factory to be aware of
+     */
+    void setMasterFuncFactory(FuncFactory masterFuncFactory) {
         this.masterFuncFactory = masterFuncFactory;
     }
 
