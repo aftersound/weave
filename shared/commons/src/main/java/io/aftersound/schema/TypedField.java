782eef8a83ada67ab69fe242eb25b410f3f7447d
jaloid <aftersound@gmail.com>
2025-02-02 09:27:43 -0800

    copy Typed/TypedField from schema-core

---
+++ shared/commons/src/main/java/io/aftersound/schema/TypedField.java
@@ -0,0 +1,42 @@
+package io.aftersound.schema;
+
+public final class TypedField<T> extends Field implements Typed<T> {
+
+    private final Class<T> cls;
+
+    public TypedField(String name, Class<T> cls) {
+        super();
+        super.setName(name);
+
+        Type type = new Type();
+        type.setName("class:" + cls.getName());
+        super.setType(type);
+        this.cls = cls;
+    }
+
+    public TypedField<T> func(String func) {
+//        super.setFunc(func);
+        return this;
+    }
+
+    public TypedField<T> source(String source) {
+//        super.setSource(source);
+        return this;
+    }
+
+    public TypedField<T> description(String description) {
+        super.setDescription(description);
+        return this;
+    }
+
+    @Override
+    public Class<T> type() {
+        return cls;
+    }
+
+    @Override
+    public T cast(Object v) {
+        return cls.isInstance(v) ? cls.cast(v) : null;
+    }
+
+}
