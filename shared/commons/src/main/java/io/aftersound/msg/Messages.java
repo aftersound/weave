28309b5fc69f980a1685df12e5b5d79582860cd7
jaloid <aftersound@gmail.com>
2025-02-05 22:57:08 -0800

    improve messages

---
+++ shared/commons/src/main/java/io/aftersound/msg/Messages.java
@@ -0,0 +1,77 @@
+package io.aftersound.msg;
+
+import java.util.*;
+
+public class Messages {
+
+    private final List<Message> messageList = new ArrayList<>();
+
+    public Messages addMessage(Message message) {
+        if (message != null) {
+            messageList.add(message);
+        }
+        return this;
+    }
+
+    public Messages addMessages(Collection<Message> messages) {
+        if (messages != null) {
+            messageList.addAll(messages);
+        }
+        return this;
+    }
+
+    public Messages addMessages(Message... messages) {
+        if (messages != null) {
+            return addMessages(Arrays.asList(messages));
+        }
+        return this;
+    }
+
+    public Messages acquire(Messages other) {
+        if (other != null) {
+            this.messageList.addAll(other.messageList);
+        }
+        return this;
+    }
+
+    public boolean hasAnyMessageWithCodes(Collection<String> codes) {
+        boolean anyMatched = false;
+        for (Message message : messageList) {
+            if (codes.contains(message.getCode())) {
+                anyMatched = true;
+                break;
+            }
+        }
+        return anyMatched;
+    }
+
+    public int size() {
+        return messageList.size();
+    }
+
+    public List<Message> list() {
+        return Collections.unmodifiableList(messageList);
+    }
+
+    public Messages getMessagesWithSeverity(String severity) {
+        Messages messagesWithSpecifiedSeverity = new Messages();
+        for (Message message : messageList) {
+            if (severity.equals(message.getSeverity())) {
+                messagesWithSpecifiedSeverity.addMessage(message);
+            }
+        }
+        return messagesWithSpecifiedSeverity;
+    }
+
+    public Message getMessage(String code) {
+        Message target = null;
+        for (Message message : messageList) {
+            if (code.equals(message.getCode())) {
+                target = message;
+                break;
+            }
+        }
+        return target;
+    }
+
+}
---
+++ shared/commons/src/main/java/io/aftersound/msg/Severity.java
@@ -0,0 +1,7 @@
+package io.aftersound.msg;
+
+public final class Severity {
+    public static final String ERROR = "Error";
+    public static final String INFO = "Info";
+    public static final String WARNING = "Warning";
+}
