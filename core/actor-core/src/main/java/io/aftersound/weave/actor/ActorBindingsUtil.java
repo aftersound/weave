5080e66faf1c6a7eea11020567d3ebc8a21b6dd4
jaloid <aftersound@gmail.com>
2025-03-15 17:07:03 -0700

    Enhance ActorBindingsUtil

--- core/actor-core/src/main/java/io/aftersound/weave/actor/ActorBindingsUtil.java
+++ core/actor-core/src/main/java/io/aftersound/weave/actor/ActorBindingsUtil.java
@@ -17,25 +17,73 @@ public final class ActorBindingsUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(ActorBindingsUtil.class);
 
     /**
-     * Load actor types and register bindings of control, actor and product
-     * @param actorClassNameList
-     *          a list of class names expected to be extended classes of ACTOR
-     * @param baseControlType
-     *          base type for CONTROL
-     * @param baseProductType
-     *          base type for PRODUCT
-     * @param tolerateIndividualException
-     *          indicator whether to tolerate exception at individual class level
-     * @param <CONTROL>
-     *          conceptual control that affects behavior/product of conceptual ACTOR
-     * @param <ACTOR>
-     *          conceptual actor which acts in according to CONTROL
-     * @param <PRODUCT>
-     *          conceptual product which ACTOR produces
-     * @return
-     *          bindings for conceptual CONTROL, ACTOR and PRODUCT
-     * @throws Exception
-     *          any exception during loading and registering
+     * Loads actor classes and registers bindings among control types, actor classes,
+     * and product types. The method allows for optional tolerance of exceptions
+     * during the individual registration of actor classes.
+     *
+     * @param <CONTROL>                   - the generic type representing the conceptual control that affects the behavior/product of the actor
+     * @param <ACTOR>                     - the generic type representing the conceptual actor that acts according to control
+     * @param <PRODUCT>                   - the generic type representing the conceptual product produced by the actor
+     * @param actorClasses                - the set of actor classes to be loaded and registered; must extend the ACTOR type
+     * @param baseControlType             - the base type representing the control type
+     * @param baseProductType             - the base type representing the product type
+     * @param tolerateIndividualException - indicates whether to tolerate individual exceptions during actor registration
+     * @return a constructed and populated {@link ActorBindings} instance containing the registered bindings
+     * @throws Exception if an error occurs during the loading and registration process and exceptions are not tolerated
+     */
+    public static <CONTROL, ACTOR, PRODUCT> ActorBindings<CONTROL, ACTOR, PRODUCT> loadActorBindings(
+            Set<Class<? extends ACTOR>> actorClasses,
+            Class<CONTROL> baseControlType,
+            Class<PRODUCT> baseProductType,
+            boolean tolerateIndividualException) throws Exception {
+        ActorBindings<CONTROL, ACTOR, PRODUCT> bindings = new ActorBindings<>();
+
+        if (actorClasses == null || actorClasses.isEmpty()) {
+            return bindings;
+        }
+
+        for (Class<? extends ACTOR> actorClass : actorClasses) {
+            try {
+                NamedType<CONTROL> controlType = extractControlType(actorClass, baseControlType);
+                NamedType<PRODUCT> productType = extractProductType(actorClass, baseProductType);
+
+                /*
+                 * Intentionally ignore actor types with subordinate control type.
+                 * Such an actor type relies on manager actor type to do it duty, while using
+                 * class loading to hand the details of duty to manager actor type
+                 */
+                if (!controlType.isSubordinate()) {
+                    bindings.register(controlType, actorClass, productType);
+                }
+            } catch (Exception e) {
+                if (tolerateIndividualException) {
+                    LOGGER.error(
+                            "{}: TOLERABLE exception occurred when attempting to register actor type {}",
+                            e,
+                            actorClass.getName()
+                    );
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        return bindings;
+    }
+
+    /**
+     * Loads actor classes and registers bindings among control types, actor classes, and product types.
+     * This method supports optional toleration of exceptions during the individual registration of actor classes.
+     *
+     * @param <CONTROL>                   - the generic type representing the conceptual control that affects the behavior/product of the actor
+     * @param <ACTOR>                     - the generic type representing the conceptual actor that acts according to control
+     * @param <PRODUCT>                   - the generic type representing the conceptual product produced by the actor
+     * @param actorClassNameList          - the list of fully qualified class names representing actor classes to be loaded
+     * @param baseControlType             - the base type representing the control type
+     * @param baseProductType             - the base type representing the product type
+     * @param tolerateIndividualException - indicates whether to tolerate individual exceptions during actor class loading
+     * @return a constructed and populated ActorBindings instance containing the registered bindings
+     * @throws Exception if an error occurs during the loading and registration process and exceptions are not tolerated
      */
     public static <CONTROL, ACTOR, PRODUCT> ActorBindings<CONTROL, ACTOR, PRODUCT> loadActorBindings(
             List<String> actorClassNameList,
@@ -67,33 +115,7 @@ public final class ActorBindingsUtil {
             }
         }
 
-        for (Class<? extends ACTOR> actorClass : actorClasses) {
-            try {
-                NamedType<CONTROL> controlType = extractControlType(actorClass, baseControlType);
-                NamedType<PRODUCT> productType = extractProductType(actorClass, baseProductType);
-
-                /*
-                 * Intentionally ignore actor types with subordinate control type.
-                 * Such an actor type relies on manager actor type to do it duty, while using
-                 * class loading to hand the details of duty to manager actor type
-                 */
-                if (!controlType.isSubordinate()) {
-                    bindings.register(controlType, actorClass, productType);
-                }
-            } catch (Exception e) {
-                if (tolerateIndividualException) {
-                    LOGGER.error(
-                            "{}: TOLERABLE exception occurred when attempting to register actor type {}",
-                            e,
-                            actorClass.getName()
-                    );
-                } else {
-                    throw e;
-                }
-            }
-        }
-
-        return bindings;
+        return loadActorBindings(actorClasses, baseControlType, baseProductType, tolerateIndividualException);
     }
 
     private static <CONTROL> NamedType<CONTROL> extractControlType(
